b557a1b2fab09054821f72c2310cd4928ec92c41
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-25 07:15:10 +0500

    MyList implemented

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,5 +1,39 @@
 package org.example;
 
-public class MyArrayList {
+public class MyArrayList implements MyList {
 
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
 }
