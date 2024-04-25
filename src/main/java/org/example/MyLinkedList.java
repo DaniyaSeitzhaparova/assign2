880557123c69aec34e00826263b72b93f08bc116
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-25 09:23:38 +0500

    Define MyNode

--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class MyLinkedList {
+    private class MyNode {
+        T data;
+        MyNode next;
+        MyNode prev;
 
+        MyNode(T data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
 }
