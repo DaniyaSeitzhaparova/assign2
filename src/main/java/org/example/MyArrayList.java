5144b66891e9234d844a5282ace44d88d439b868
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-26 08:10:10 +0500

    Sorting method added to MyArrayList

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -4,92 +4,105 @@ import java.util.Iterator;
 
 
 public abstract class MyArrayList<T> implements MyList<T> {
-        private static final int capacity = 10;
-        private Object[] elements;
-        private int size;
+    private static final int capacity = 10;
+    private Object[] elements;
+    private int size;
 
-        public MyArrayList() {
-            this.elements = new Object[capacity];
-            this.size = 0;
+    public MyArrayList() {
+        this.elements = new Object[capacity];
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        if (size == elements.length) {
+            increaseCapacity();
+        }
+        elements[size++] = element;
+    }
+
+    private void increaseCapacity() {
+        Object[] newElements = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null; // Optional: Set the last element to null to aid garbage collection
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        return (T) elements[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return (Iterator<T>) new MyArrayListIterator<>(this);
+    }
+
+    // Define a custom iterator for MyArrayList
+    private static class MyArrayListIterator<T> implements Iterator<T> {
+        private MyArrayList<T> list;
+        private int currentIndex;
+
+        public MyArrayListIterator(MyArrayList<T> list) {
+            this.list = list;
+            this.currentIndex = 0;
         }
 
         @Override
-        public void add(T element) {
-            if (size == elements.length) {
-                increaseCapacity();
-            }
-            elements[size++] = element;
-        }
-
-        private void increaseCapacity() {
-            Object[] newElements = new Object[elements.length * 2];
-            System.arraycopy(elements, 0, newElements, 0, elements.length);
-            elements = newElements;
+        public boolean hasNext() {
+            return currentIndex < list.size();
         }
 
         @Override
-        public void remove(int index) {
-            if (index < 0 || index >= size) {
-                throw new IndexOutOfBoundsException(index);
+        public T next() {
+            if (!hasNext()) {
+                throw new RuntimeException("No such element");
             }
-            for (int i = index; i < size - 1; i++) {
-                elements[i] = elements[i + 1];
-            }
-            elements[--size] = null;
-        }
-
-        @Override
-        public T get(int index) {
-            if (index < 0 || index >= size) {
-                throw new IndexOutOfBoundsException(index);
-            }
-            return (T) elements[index];
-        }
-
-        @Override
-        public int size() {
-            return size;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return size == 0;
-        }
-
-        @Override
-        public void clear() {
-            for (int i = 0; i < size; i++) {
-                elements[i] = null;
-            }
-            size = 0;
-        }
-
-        @Override
-        public Iterator<T> iterator() {
-            return (Iterator<T>) new MyArrayListIterator<T>(this);
-        }
-
-        private static class MyArrayListIterator<T> {
-            private MyArrayList<T> list;
-            private int currentIndex;
-
-            public MyArrayListIterator(MyArrayList<T> list) {
-                this.list = list;
-                this.currentIndex = 0;
-            }
-
-            @Override
-            public boolean hasNext() {
-                return currentIndex < list.size();
-            }
-
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new RuntimeException("No such element");
-                }
-                return list.get(currentIndex++);
-            }
+            return list.get(currentIndex++);
         }
     }
 
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (elements[j].compareTo(elements[j + 1]) > 0) {
+                    Object temp = elements[j];
+                    elements[j] = elements[j + 1];
+                    elements[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
