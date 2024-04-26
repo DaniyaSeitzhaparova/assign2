6c7a22e8325d2d0ec2698d30235466cbd0c10da6
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-04-26 08:02:30 +0500

    Implemented methods of MyLinkedList

--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Iterator;
+
 public class MyLinkedList<T> implements MyList<T> {
     private class MyNode {
         T data;
@@ -11,8 +13,92 @@ public class MyLinkedList<T> implements MyList<T> {
             this.next = null;
             this.prev = null;
         }
-
-        private MyNode head;
-        private MyNode tail;
-        private int size;
     }
+
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        MyNode newNode = new MyNode(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        MyNode current = getNode(index);
+        if (current.prev == null) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+        if (current.next == null) {
+            tail = current.prev;
+        } else {
+            current.next.prev = current.prev;
+        }
+        size--;
+    }
+
+    private MyNode getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        MyNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    @Override
+    public T get(int index) {
+        MyNode current = getNode(index);
+        return current.data;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        while (head != null) {
+            MyNode temp = head;
+            head = head.next;
+            temp.next = null;
+            temp.prev = null;
+        }
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+}
